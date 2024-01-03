689bfd591bac3e0958fd464da2bb082257069ee6
Rafael Barros <rafael.barros@lymtech.com.br>
2024-01-03 13:16:42 -0300

    Método de adição e edição de Fluxo

--- monitor_de_excepciones/flow/views.py
+++ monitor_de_excepciones/flow/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -7,6 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from flow.models import Flow, Notification
+from .forms import FlowForm
 from authorization.models import Autorization
 
 class DispatchLoginRequiredMixin(View):
@@ -26,10 +28,34 @@ class CheckFlow(DispatchLoginRequiredMixin, View):
         validate = Flow.objects.filter()
 
 class AddFlow(DispatchLoginRequiredMixin, View):
-    pass
+    def newFlow(request):
+        if request.method == 'POST':
+            form = FlowForm(request.POST)
+            if form.is_valid():
+                new_flow = form.save(commit=False)
+                new_flow.owner = request.user
+                new_flow.save()
+                return redirect('flow:home')
+            else:
+                form = FlowForm()
+            
+            return render(request, 'flow:home', {'form': form})
 
 class EditFlow(DispatchLoginRequiredMixin, View):
-    pass
+    def editFlow(request, id_processo):
+        flow = get_object_or_404(Flow, pk=id_processo, show=True, owner=request.user)
+        form_action = reverse('flow:edit', args=(id_processo))
+
+        if request.method == 'POST':
+            form = FlowForm(request.POST, request.FILES, instance=flow)
+
+            context = {'form': form, 'form_action': form_action}
+
+            if form.is_valid():
+                update_flow = form.save()
+                return redirect('flow:edit', id_processo=update_flow.pk)
+            
+            return render(request, 'flow:home', context)
 
 class CheckNotif(DispatchLoginRequiredMixin, View):
     pass
