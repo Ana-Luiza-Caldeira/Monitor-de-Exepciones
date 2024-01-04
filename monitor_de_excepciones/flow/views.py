534ecf7a61d5ee5aee0698f767129c65a3010988
Rafael Barros <rafael.barros@lymtech.com.br>
2024-01-04 09:23:10 -0300

    Mudança no import das Views

--- monitor_de_excepciones/flow/views.py
+++ monitor_de_excepciones/flow/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from flow.models import Flow, Notification
-from .forms import FlowForm
+from .forms import FlowForm, NotificationForm
 from authorization.models import Authorization
 
 class DispatchLoginRequiredMixin(View):
@@ -41,47 +41,85 @@ class Index(DispatchLoginRequiredMixin, View):
 
         return render(request, 'index.html', context)
 
-# class CheckFlow(DispatchLoginRequiredMixin, View):
-#     def showFlow(self, *args, **kwargs):
-#         validate = Flow.objects.filter()
+class CheckFlow(DispatchLoginRequiredMixin, View):
+    def showFlow(self, *args, **kwargs):
+        validate = Flow.objects.filter(owner=self.user)
 
-# class AddFlow(DispatchLoginRequiredMixin, View):
-#     def newFlow(request):
-#         if request.method == 'POST':
-#             form = FlowForm(request.POST)
-#             if form.is_valid():
-#                 new_flow = form.save(commit=False)
-#                 new_flow.owner = request.user
-#                 new_flow.save()
-#                 return redirect('flow:home')
-#             else:
-#                 form = FlowForm()
+        context = {
+            'flows': validate
+        }
+
+        return render(self.request, 'flow/home.html', context)
+
+class AddFlow(DispatchLoginRequiredMixin, View):
+    def newFlow(request):
+        if request.method == 'POST':
+            form = FlowForm(request.POST)
+            if form.is_valid():
+                new_flow = form.save(commit=False)
+                new_flow.owner = request.user
+                new_flow.save()
+                return redirect('flow:home')
+            else:
+                form = FlowForm()
             
-#             return render(request, 'flow:home', {'form': form})
+            return render(request, 'flow:home', {'form': form})
 
-# class EditFlow(DispatchLoginRequiredMixin, View):
-#     def editFlow(request, process_id):
-#         flow = get_object_or_404(Flow, pk=process_id, show=True, owner=request.user)
-#         form_action = reverse('flow:edit', args=(process_id))
+class EditFlow(DispatchLoginRequiredMixin, View):
+    def editFlow(request, process_id):
+        flow = get_object_or_404(Flow, pk=process_id, show=True, owner=request.user)
+        form_action = reverse('flow:edit', args=(process_id))
 
-#         if request.method == 'POST':
-#             form = FlowForm(request.POST, request.FILES, instance=flow)
+        if request.method == 'POST':
+            form = FlowForm(request.POST, request.FILES, instance=flow)
 
-#             context = {'form': form, 'form_action': form_action}
+            context = {'form': form, 'form_action': form_action}
 
-#             if form.is_valid():
-#                 update_flow = form.save()
-#                 return redirect('flow:edit', process_id=update_flow.pk)
+            if form.is_valid():
+                update_flow = form.save()
+                return redirect('flow:edit', process_id=update_flow.pk)
             
-#             return render(request, 'flow:home', context)
+            return render(request, 'flow:home', context)
 
-# class CheckNotif(DispatchLoginRequiredMixin, View):
-#     pass
+class CheckNotif(DispatchLoginRequiredMixin, View):
+    def showNotif(self, *args, **kwargs):
+        validate = Notification.objects.filter(owner=self.user)
 
-# class AddNotif(DispatchLoginRequiredMixin, View):
-#     pass
+        context = {
+            'notifs': validate
+        }
 
-# class EditNotif(DispatchLoginRequiredMixin, View):
-#     pass
+        return render(self.request, 'flow/home.html', context)  
+
+class AddNotif(DispatchLoginRequiredMixin, View):
+    def newNotification(request):
+        if request.method == 'POST':
+            form = NotificationForm(request.POST)
+            if form.is_valid():
+                new_notification = form.save(commit=False)
+                new_notification.owner = request.user
+            #   new_notification.flow = 
+                new_notification.save()
+                return redirect('flow:home')
+            else:
+                form = NotificationForm()
+            
+            return render(request, 'flow:home', {'form': form})
+
+class EditNotif(DispatchLoginRequiredMixin, View):
+    def editNotif(request, transaction_id):
+        notif = get_object_or_404(Flow, pk=transaction_id, show=True, owner=request.user)
+        form_action = reverse('flow:edit', args=(transaction_id))
+
+        if request.method == 'POST':
+            form = NotificationForm(request.POST, request.FILES, instance=notif)
+
+            context = {'form': form, 'form_action': form_action}
+
+            if form.is_valid():
+                update_notif = form.save()
+                return redirect('flow:edit', transaction_id=update_notif.pk)
+            
+            return render(request, 'flow:home', context)
 
 
